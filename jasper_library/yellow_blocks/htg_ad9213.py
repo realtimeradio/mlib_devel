4211475f5d0c254641e309b111dcbbd90cb7102e
Jack <jack@realtimeradio.co.uk>
2021-09-19 13:45:36 +0000

    Add skeleton yellow block for HTG AD9213

---
+++ jasper_library/yellow_blocks/htg_ad9213.py
@@ -0,0 +1,191 @@
+import os
+from .yellow_block import YellowBlock
+from verilog import VerilogModule
+from constraints import PortConstraint, ClockConstraint, RawConstraint
+
+from math import ceil, floor
+
+class htg_ad9213(YellowBlock):
+    def initialize(self):
+        print(self.source_path)
+        if not self.source_path:
+            self.source_path = os.environ['HDL_ROOT']
+
+        print(self.source_path)
+        hdl_path = os.path.join(self.source_path, 'src', 'hdl')
+        bd_path = os.path.join(self.source_path, 'src', 'bd')
+
+        self.add_source('htg_ad9213/htg_ad9213_quad_top.v')
+        #self.add_source(os.path.join(hdl_path, 'd_ff.v'))
+        #self.add_source(os.path.join(hdl_path, 'ad_3w_spi.v'))
+        #self.add_source(os.path.join(hdl_path, 'top.v'))
+        #self.add_source(os.path.join(hdl_path, 'jesd204_ad9213_demapper.v'))
+
+        self.provides = []
+        # LIES: This core doesn't actually provide clocks other than 0-degrees
+        if self.use_fmc_a:
+            self.provides += ['fmc_a_clk', 'fmc_a_clk90', 'fmc_a_clk180', 'fmc_a_clk270']
+        if self.use_fmc_b:
+            self.provides += ['fmc_b_clk', 'fmc_b_clk90', 'fmc_b_clk180', 'fmc_b_clk270']
+        if self.use_fmc_c:
+            self.provides += ['fmc_c_clk', 'fmc_c_clk90', 'fmc_c_clk180', 'fmc_c_clk270']
+        if self.use_fmc_d:
+            self.provides += ['fmc_d_clk', 'fmc_d_clk90', 'fmc_d_clk180', 'fmc_d_clk270']
+
+        self.bd = os.path.join(bd_path, 'adc_test.tcl')
+
+    def _add_one_fmc_interface(self, inst, fmc):
+        """
+        Add all ports associated with ``fmc`` to VerilogModule ``inst``.
+
+        :param inst: Verilog instance to which ports should be added.
+        :type inst: VerilogModule
+
+        :param fmc: FMC port. Either 'a', 'b', 'c', or 'd'
+        :type fmc: str
+        """
+
+        assert fmc in ['a', 'b', 'c', 'd']
+
+        # External Ports
+        inst.add_port('hmc_' + fmc + '_sync',           self.expand_name('hmc_' + fmc + '_sync'),           parent_port=True, dir='out')
+        inst.add_port('hmc_' + fmc + '_reset',          self.expand_name('hmc_' + fmc + '_reset'),          parent_port=True, dir='out')
+        inst.add_port('hmc_' + fmc + '_gpio1',          self.expand_name('hmc_' + fmc + '_gpio1'),          parent_port=True, dir='inout')
+        inst.add_port('hmc_' + fmc + '_gpio2',          self.expand_name('hmc_' + fmc + '_gpio2'),          parent_port=True, dir='inout')
+        inst.add_port('hmc_' + fmc + '_gpio3',          self.expand_name('hmc_' + fmc + '_gpio3'),          parent_port=True, dir='inout')
+        inst.add_port('hmc_' + fmc + '_gpio4',          self.expand_name('hmc_' + fmc + '_gpio4'),          parent_port=True, dir='inout')
+        inst.add_port('spi_' + fmc + '_slen_hmc7044',   self.expand_name('spi_' + fmc + '_slen_hmc7044'),   parent_port=True, dir='out')
+        inst.add_port('spi_' + fmc + '_cs_adf4371',     self.expand_name('spi_' + fmc + '_cs_adf4371'),     parent_port=True, dir='out')
+        inst.add_port('spi_' + fmc + '_csb_ad9213',     self.expand_name('spi_' + fmc + '_csb_ad9213'),     parent_port=True, dir='out')
+        inst.add_port('spi_' + fmc + '_clk',            self.expand_name('spi_' + fmc + '_clk'),            parent_port=True, dir='out')
+        inst.add_port('spi_' + fmc + '_data',           self.expand_name('spi_' + fmc + '_data'),           parent_port=True, dir='inout')
+        inst.add_port('adc_' + fmc + '_pdwn',           self.expand_name('adc_' + fmc + '_pdwn'),           parent_port=True, dir='out')
+        inst.add_port('adc_' + fmc + '_rstb',           self.expand_name('adc_' + fmc + '_rstb'),           parent_port=True, dir='out')
+        inst.add_port('adc_' + fmc + '_gpio0',          self.expand_name('adc_' + fmc + '_gpio0'),          parent_port=True, dir='inout')
+        inst.add_port('adc_' + fmc + '_gpio1',          self.expand_name('adc_' + fmc + '_gpio1'),          parent_port=True, dir='inout')
+        inst.add_port('adc_' + fmc + '_gpio2',          self.expand_name('adc_' + fmc + '_gpio2'),          parent_port=True, dir='inout')
+        inst.add_port('adc_' + fmc + '_gpio3',          self.expand_name('adc_' + fmc + '_gpio3'),          parent_port=True, dir='inout')
+        inst.add_port('adc_' + fmc + '_gpio4',          self.expand_name('adc_' + fmc + '_gpio4'),          parent_port=True, dir='inout')
+        inst.add_port('jesd_' + fmc + '_ref_clk0_p',    self.expand_name('jesd_' + fmc + '_ref_clk0_p'),    parent_port=True, dir='in')
+        inst.add_port('jesd_' + fmc + '_ref_clk0_n',    self.expand_name('jesd_' + fmc + '_ref_clk0_n'),    parent_port=True, dir='in')
+        inst.add_port('jesd_' + fmc + '_ref_clk1_p',    self.expand_name('jesd_' + fmc + '_ref_clk1_p'),    parent_port=True, dir='in')
+        inst.add_port('jesd_' + fmc + '_ref_clk1_n',    self.expand_name('jesd_' + fmc + '_ref_clk1_n'),    parent_port=True, dir='in')
+        inst.add_port('jesd_' + fmc + '_sysref1_clk_p', self.expand_name('jesd_' + fmc + '_sysref1_clk_p'), parent_port=True, dir='in')
+        inst.add_port('jesd_' + fmc + '_sysref1_clk_n', self.expand_name('jesd_' + fmc + '_sysref1_clk_n'), parent_port=True, dir='in')
+        inst.add_port('jesd_' + fmc + '_syncinb_p',     self.expand_name('jesd_' + fmc + '_syncinb_p'),     parent_port=True, dir='out')
+        inst.add_port('jesd_' + fmc + '_serdes_0_p',    self.expand_name('jesd_' + fmc + '_serdes_0_p'),    parent_port=True, dir='in', width=8)
+        inst.add_port('jesd_' + fmc + '_serdes_0_n',    self.expand_name('jesd_' + fmc + '_serdes_0_n'),    parent_port=True, dir='in', width=8)
+        inst.add_port('jesd_' + fmc + '_serdes_1_p',    self.expand_name('jesd_' + fmc + '_serdes_1_p'),    parent_port=True, dir='in', width=8)
+        inst.add_port('jesd_' + fmc + '_serdes_1_n',    self.expand_name('jesd_' + fmc + '_serdes_1_n'),    parent_port=True, dir='in', width=8)
+        # Simulink Ports
+        for i in range(32):
+            inst.add_port('adc_' + fmc + '_dout_' + str(i), self.expand_name('adc_' + fmc + '_dout_' + str(i)), width=12, parent_port=False)
+        # Data output clock
+        inst.add_port('adc_' + fmc + '_clkout', 'fmc_' + fmc + '_clk', parent_port=False)
+        
+    def expand_name(self, name):
+        return self.fullname + '_' + name
+
+    def modify_top(self,top):
+        inst = top.get_instance(entity='htg_ad9213_quad_top', name=self.expand_name('inst'))
+
+        # Common ports
+        # 200M Clk
+        inst.add_port('clk_200_p', self.expand_name('clk_200_p'), dir='in', parent_port=True)
+        inst.add_port('clk_200_n', self.expand_name('clk_200_p'), dir='in', parent_port=True)
+
+        # Simulink reset input
+        inst.add_port('reset', self.expand_name('reset'))
+
+        # Uart pins
+        if self.use_uart:
+            inst.add_port('uart_txd', self.expand_name('uart_txd'), dir='in', parent_port=True)
+            inst.add_port('uart_rxd', self.expand_name('uart_rxd'), dir='out', parent_port=True)
+        else:
+            inst.add_port('uart_txd', "1'b0", dir='in', parent_port=False)
+
+        # TODO. LEDs, Lock signals, etc.
+
+        if self.use_fmc_a:
+            self._add_one_fmc_interface(inst, 'a')
+        if self.use_fmc_b:
+            self._add_one_fmc_interface(inst, 'b')
+        if self.use_fmc_c:
+            self._add_one_fmc_interface(inst, 'c')
+        if self.use_fmc_d:
+            self._add_one_fmc_interface(inst, 'd')
+        
+    def gen_tcl_cmds(self):
+        tcl_cmds = {}
+        tcl_cmds['pre_synth'] = ['source %s' % self.bd]
+        return tcl_cmds
+
+    def _gen_constraints_one_fmc_interface(self, fmc):
+        """
+        Return all constraints associated with ``fmc``.
+
+        :param fmc: FMC port. Either 'a', 'b', 'c', or 'd'
+        :type fmc: str
+        """
+
+        assert fmc in ['a', 'b', 'c', 'd']
+
+        cons = []
+        cons += [PortConstraint(self.expand_name('hmc_' + fmc + '_sync'),           'fmc_' + fmc + '_la_n', iogroup_index=3)]
+        cons += [PortConstraint(self.expand_name('hmc_' + fmc + '_reset'),          'fmc_' + fmc + '_la_p', iogroup_index=3)]
+        cons += [PortConstraint(self.expand_name('hmc_' + fmc + '_gpio1'),          'fmc_' + fmc + '_la_p', iogroup_index=2)]
+        cons += [PortConstraint(self.expand_name('hmc_' + fmc + '_gpio2'),          'fmc_' + fmc + '_la_p', iogroup_index=0)]
+        cons += [PortConstraint(self.expand_name('hmc_' + fmc + '_gpio3'),          'fmc_' + fmc + '_la_n', iogroup_index=1)]
+        cons += [PortConstraint(self.expand_name('hmc_' + fmc + '_gpio4'),          'fmc_' + fmc + '_la_p', iogroup_index=5)]
+
+        cons += [PortConstraint(self.expand_name('spi_' + fmc + '_clk'),            'fmc_' + fmc + '_la_n', iogroup_index=2)]
+        cons += [PortConstraint(self.expand_name('spi_' + fmc + '_data'),           'fmc_' + fmc + '_la_p', iogroup_index=1)]
+        cons += [PortConstraint(self.expand_name('spi_' + fmc + '_slen_hmc7044'),   'fmc_' + fmc + '_la_n', iogroup_index=0)]
+        cons += [PortConstraint(self.expand_name('spi_' + fmc + '_cs_adf4371'),     'fmc_' + fmc + '_la_n', iogroup_index=16)]
+        cons += [PortConstraint(self.expand_name('spi_' + fmc + '_csb_ad9213'),     'fmc_' + fmc + '_la_p', iogroup_index=6)]
+
+        cons += [PortConstraint(self.expand_name('adc_' + fmc + '_pdwn'),           'fmc_' + fmc + '_la_n', iogroup_index=7)]
+        cons += [PortConstraint(self.expand_name('adc_' + fmc + '_rstb'),           'fmc_' + fmc + '_la_p', iogroup_index=16)]
+        cons += [PortConstraint(self.expand_name('adc_' + fmc + '_gpio0'),          'fmc_' + fmc + '_la_n', iogroup_index=5)]
+        cons += [PortConstraint(self.expand_name('adc_' + fmc + '_gpio1'),          'fmc_' + fmc + '_la_n', iogroup_index=4)]
+        cons += [PortConstraint(self.expand_name('adc_' + fmc + '_gpio2'),          'fmc_' + fmc + '_la_n', iogroup_index=6)]
+        cons += [PortConstraint(self.expand_name('adc_' + fmc + '_gpio3'),          'fmc_' + fmc + '_la_p', iogroup_index=4)]
+        cons += [PortConstraint(self.expand_name('adc_' + fmc + '_gpio4'),          'fmc_' + fmc + '_la_n', iogroup_index=12)]
+
+        cons += [PortConstraint(self.expand_name('jesd_' + fmc + '_ref_clk0_p'),    'fmc_' + fmc + '_gbtclk_m2c_p', iogroup_index=0)]
+        cons += [PortConstraint(self.expand_name('jesd_' + fmc + '_ref_clk0_n'),    'fmc_' + fmc + '_gbtclk_m2c_n', iogroup_index=0)]
+        cons += [PortConstraint(self.expand_name('jesd_' + fmc + '_ref_clk1_p'),    'fmc_' + fmc + '_gbtclk_m2c_p', iogroup_index=1)]
+        cons += [PortConstraint(self.expand_name('jesd_' + fmc + '_ref_clk1_n'),    'fmc_' + fmc + '_gbtclk_m2c_n', iogroup_index=1)]
+        cons += [PortConstraint(self.expand_name('jesd_' + fmc + '_sysref1_clk_p'), 'fmc_' + fmc + '_clk_m2c_p', iogroup_index=0)]
+        cons += [PortConstraint(self.expand_name('jesd_' + fmc + '_sysref1_clk_n'), 'fmc_' + fmc + '_clk_m2c_n', iogroup_index=0)]
+        cons += [PortConstraint(self.expand_name('jesd_' + fmc + '_syncinb_p'),     'fmc_' + fmc + '_la_p', iogroup_index=10)]
+        # Set by IP
+        #cons += [PortConstraint(self.expand_name('jesd_' + fmc + '_serdes_0_p'),    'fmc_' + fmc + 'XXXXX', iogroup_index=2)]
+        #cons += [PortConstraint(self.expand_name('jesd_' + fmc + '_serdes_0_n'),    'fmc_' + fmc + 'XXXXX', iogroup_index=2)]
+        #cons += [PortConstraint(self.expand_name('jesd_' + fmc + '_serdes_1_p'),    'fmc_' + fmc + 'XXXXX', iogroup_index=2)]
+        #cons += [PortConstraint(self.expand_name('jesd_' + fmc + '_serdes_1_n'),    'fmc_' + fmc + 'XXXXX', iogroup_index=2)]
+        return cons
+
+
+    def gen_constraints(self):
+        cons = []
+        # Common constraints
+        cons += [PortConstraint(self.expand_name('clk_200_p'), 'sys_clk_200_p')]
+        cons += [PortConstraint(self.expand_name('clk_200_n'), 'sys_clk_200_n')]
+
+        if self.use_uart:
+            cons += [PortConstraint(self.expand_name('uart_txd'), 'usb_rx')]
+            cons += [PortConstraint(self.expand_name('uart_rxd'), 'usb_tx')]
+
+        if self.use_fmc_a:
+            cons += self._gen_constraints_one_fmc_interface('a')
+        if self.use_fmc_b:
+            cons += self._gen_constraints_one_fmc_interface('b')
+        if self.use_fmc_c:
+            cons += self._gen_constraints_one_fmc_interface('c')
+        if self.use_fmc_d:
+            cons += self._gen_constraints_one_fmc_interface('d')
+
+        return cons
+
+        
---
+++ xps_library/htg_ad9213_mask.m
@@ -0,0 +1,119 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                             %
+%   Center for Astronomy Signal Processing and Electronics Research           %
+%   http://seti.ssl.berkeley.edu/casper/                                      %
+%   Copyright (C) 2013 David MacMahon
+%                                                                             %
+%   This program is free software; you can redistribute it and/or modify      %
+%   it under the terms of the GNU General Public License as published by      %
+%   the Free Software Foundation; either version 2 of the License, or         %
+%   (at your option) any later version.                                       %
+%                                                                             %
+%   This program is distributed in the hope that it will be useful,           %
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of            %
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             %
+%   GNU General Public License for more details.                              %
+%                                                                             %
+%   You should have received a copy of the GNU General Public License along   %
+%   with this program; if not, write to the Free Software Foundation, Inc.,   %
+%   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.               %
+%                                                                             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function htg_ad9213_mask(blk, use_fmc_a, use_fmc_b, use_fmc_c, use_fmc_d)
+
+try
+  clog('adc16_init: pre same_state','trace');
+
+  check_mask_type(blk, 'htg_ad9213');
+  delete_lines(blk);
+  %clean_blocks(blk)
+
+  gw_name = clear_name(blk);
+
+  fmcs = {'a', 'b', 'c', 'd'};
+
+  x =  0;
+  y = 20;
+  in_port_num = 1;
+  out_port_num = 1;
+
+
+  inport_name  = sprintf('reset');
+  gateway_name = sprintf('%s_reset', gw_name);
+  outport_name = sprintf('reset_term');
+
+  inport_pos  = [x+ 20, y,   x+ 20+30, y+14];
+  gateway_pos = [x+100, y-3, x+100+70, y+17];
+  outport_pos = [x+210, y,   x+210+30, y+14];
+
+  reuse_block(blk, inport_name, 'built-in/inport', ...
+  'Port', num2str(in_port_num), ...
+  'Position', inport_pos);
+
+  reuse_block(blk, gateway_name, 'xbsIndex_r4/Gateway Out', ...
+     'Position', gateway_pos);
+
+  reuse_block(blk, outport_name, 'built-in/Terminator', ...
+     'Position', outport_pos);
+  
+  add_line(blk, [inport_name,  '/1'], [gateway_name, '/1']);
+  add_line(blk, [gateway_name, '/1'], [outport_name, '/1']);
+  
+  in_port_num = in_port_num + 1;
+  y = y + 50;
+  
+  for fmc_cell=fmcs
+    fmc = fmc_cell{1};
+    if strcmp(fmc, 'a')
+      use_fmc = use_fmc_a;
+    elseif strcmp(fmc, 'b')
+      use_fmc = use_fmc_b;
+    elseif strcmp(fmc, 'c')
+      use_fmc = use_fmc_c;
+    elseif strcmp(fmc, 'd')
+      use_fmc = use_fmc_d;
+    end
+    if use_fmc == 1
+      for lane=0:31
+        
+        inport_name  = sprintf('adc_%s_sim%d', fmc, lane);
+        gateway_name = sprintf('%s_adc_%s_dout%d', gw_name, fmc, lane);
+        outport_name = sprintf('adc_%s_dout%d', fmc, lane);
+
+        inport_pos  = [x+ 20, y,   x+ 20+30, y+14];
+        gateway_pos = [x+100, y-3, x+100+70, y+17];
+        outport_pos = [x+210, y,   x+210+30, y+14];
+
+        reuse_block(blk, inport_name, 'built-in/inport', ...
+          'Port', num2str(in_port_num), ...
+          'Position', inport_pos);
+
+        reuse_block(blk, gateway_name, 'xbsIndex_r4/Gateway In', ...
+          'arith_type', 'Signed', ...
+          'n_bits', '12', ...
+          'bin_pt', '11', ...
+          'Position', gateway_pos);
+
+        reuse_block(blk, outport_name, 'built-in/outport', ...
+          'Port', num2str(out_port_num), ...
+          'Position', outport_pos);
+
+        add_line(blk, [inport_name,  '/1'], [gateway_name, '/1']);
+        add_line(blk, [gateway_name, '/1'], [outport_name, '/1']);
+
+        in_port_num = in_port_num + 1;
+        out_port_num = out_port_num + 1;
+        y = y + 50;
+      end % lanes
+      % Increment column
+      x = x + 210+30+50;
+      y = 20 + 50;
+    end % fmc enabled
+  end % fmcs
+  clean_blocks(blk)
+catch ex
+  dump_and_rethrow(ex);
+end % try/catch
+end % function
