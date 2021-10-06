349762f4f92ad0c481717cf10de20349d496966e
Jack <jack@realtimeradio.co.uk>
2021-10-06 12:53:16 +0000

    Change MMCM parameters to fit allowed ranges

--- jasper_library/hdl_sources/htg_ad9213/htg_ad9213_mmcm.v
+++ jasper_library/hdl_sources/htg_ad9213/htg_ad9213_mmcm.v
@@ -20,10 +20,10 @@ module htg_ad9213_mmcm (
 
   MMCM_BASE #( 
    .BANDWIDTH          ("OPTIMIZED"), // Jitter programming ("HIGH","LOW","OPTIMIZED")
-   .CLKFBOUT_MULT_F    (10), // Multiply value for all CLKOUT (5.0-64.0).
+   .CLKFBOUT_MULT_F    (5), // Multiply value for all CLKOUT (5.0-64.0).
    .CLKFBOUT_PHASE     (0.0), 
    .CLKIN1_PERIOD      (5.0), // Clock is 200 MHz
-   .CLKOUT0_DIVIDE_F   (6.4), // Divide amount for CLKOUT0 (1.000-128.000).
+   .CLKOUT0_DIVIDE_F   (3.2), // Divide amount for CLKOUT0 (1.000-128.000).
    .CLKOUT0_DUTY_CYCLE (0.5),
    .CLKOUT1_DUTY_CYCLE (0.5),
    .CLKOUT2_DUTY_CYCLE (0.5),
