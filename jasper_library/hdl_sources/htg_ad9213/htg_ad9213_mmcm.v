01ae3e28e63617263c995a96827349339066be41
Jonathon Kocz <1220894+jkocz@users.noreply.github.com>
2021-10-09 20:39:32 -0700

    update htg_ad9213 mmcm to 250mhz

--- jasper_library/hdl_sources/htg_ad9213/htg_ad9213_mmcm.v
+++ jasper_library/hdl_sources/htg_ad9213/htg_ad9213_mmcm.v
@@ -23,7 +23,7 @@ module htg_ad9213_mmcm (
    .CLKFBOUT_MULT_F    (5), // Multiply value for all CLKOUT (5.0-64.0).
    .CLKFBOUT_PHASE     (0.0), 
    .CLKIN1_PERIOD      (5.0), // Clock is 200 MHz
-   .CLKOUT0_DIVIDE_F   (3.2), // Divide amount for CLKOUT0 (1.000-128.000).
+   .CLKOUT0_DIVIDE_F   (4.0), // Divide amount for CLKOUT0 (1.000-128.000).
    .CLKOUT0_DUTY_CYCLE (0.5),
    .CLKOUT1_DUTY_CYCLE (0.5),
    .CLKOUT2_DUTY_CYCLE (0.5),
