cffb234a83b4ae1d466f9fa29a8e1088e73cdf0c
JackH <jack@realtimeradio.co.uk>
2021-10-15 10:30:19 -0700

    environment controls
    Use CASPER_SKIP_STARTUP_M (defined to any non-zero-length value)
    to skip running startup.m (except the bit which
    runs casper_startup.m). Seemingly in unsupported versions of Ubuntu,
    running the startup script after matlab has started works, but including
    the startup.m script in the boot process causes MATLAB to lock up. :-S
    
    Use CASPER_USE_XILINX_CAST (defined to any non-zero-length value) to
    not use CASPER convert blocks in the convert_of or bus_convert modules.
    I seem to often want to turn off these when trying to reduce power / area /
    increase speed. I don't know why the CASPER custom block was ever created,
    but an environment switch seems more collaboration-friendly than just
    removing this block.

--- startup.m
+++ startup.m
@@ -1,27 +1,29 @@
-warning off Simulink:SL_SaveWithParameterizedLinks_Warning
-warning off Simulink:SL_LoadMdlParameterizedLink
-warning off Simulink:Engine:SaveWithParameterizedLinks_Warning
-warning off Simulink:Engine:SaveWithDisabledLinks_Warning
-warning off Simulink:Commands:LoadMdlParameterizedLink
-
-if length(getenv('XILINX_PATH')) == 0
-  setenv('XILINX_PATH', regexprep(getenv('XILINX'),'/ISE$',''));
-end
-addpath([getenv('XILINX_PATH'), '/ISE/sysgen/util/']);
-addpath([getenv('XILINX_PATH'), '/ISE/sysgen/bin/lin64']);
-addpath([getenv('MLIB_DEVEL_PATH'), '/casper_library']);
-addpath([getenv('MLIB_DEVEL_PATH'), '/xps_library']);
-xlAddSysgen([getenv('XILINX_PATH'), '/ISE'])
-%sysgen_startup
-% If CASPER_BACKPORT is in the environment with non-zero length, then force
-% block reuse and do NOT preload CASPER libraries.  This prevents problems when
-% saving libraries in older Simulink formats (aka "backporting"), but should
-% NOT be used for normal development.
-if length(getenv('CASPER_BACKPORT')) > 0
-  casper_force_reuse_block = 1;
-else
-  load_system('casper_library');
-  load_system('xps_library');
+if length(getenv('CASPER_SKIP_STARTUP_M')) == 0
+  warning off Simulink:SL_SaveWithParameterizedLinks_Warning
+  warning off Simulink:SL_LoadMdlParameterizedLink
+  warning off Simulink:Engine:SaveWithParameterizedLinks_Warning
+  warning off Simulink:Engine:SaveWithDisabledLinks_Warning
+  warning off Simulink:Commands:LoadMdlParameterizedLink
+  
+  if length(getenv('XILINX_PATH')) == 0
+    setenv('XILINX_PATH', regexprep(getenv('XILINX'),'/ISE$',''));
+  end
+  addpath([getenv('XILINX_PATH'), '/ISE/sysgen/util/']);
+  addpath([getenv('XILINX_PATH'), '/ISE/sysgen/bin/lin64']);
+  addpath([getenv('MLIB_DEVEL_PATH'), '/casper_library']);
+  addpath([getenv('MLIB_DEVEL_PATH'), '/xps_library']);
+  xlAddSysgen([getenv('XILINX_PATH'), '/ISE'])
+  %sysgen_startup
+  % If CASPER_BACKPORT is in the environment with non-zero length, then force
+  % block reuse and do NOT preload CASPER libraries.  This prevents problems when
+  % saving libraries in older Simulink formats (aka "backporting"), but should
+  % NOT be used for normal development.
+  if length(getenv('CASPER_BACKPORT')) > 0
+    casper_force_reuse_block = 1;
+  else
+    load_system('casper_library');
+    load_system('xps_library');
+  end
 end
 
 casper_startup_dir = getenv('CASPER_STARTUP_DIR');
