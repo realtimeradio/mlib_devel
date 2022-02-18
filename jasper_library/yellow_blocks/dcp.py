a35aed16ad536702c8cc51d343f72008f6fbaa1e
Jack <jack@realtimeradio.co.uk>
2022-02-18 17:21:44 +0000

    Read DCP checkpoint after importing
    Just importing doesn't seem to result in black-boxes being correctly
    linked. Possibly this is not the right solution, and something like `scope_to_cell`
    would be more appropriate. But, this seems to work.

--- jasper_library/yellow_blocks/dcp.py
+++ jasper_library/yellow_blocks/dcp.py
@@ -1,6 +1,13 @@
+import os
 from .yellow_block import YellowBlock
 
 class dcp(YellowBlock):
     def initialize(self):
         self.add_source(self.dcp_file)
+
+    def gen_tcl_cmds(self):
+       tcl_cmds = {}
+       cmd = 'read_checkpoint [get_files %s]' % os.path.basename(self.dcp_file)
+       tcl_cmds['post_synth'] = [cmd] # After opening main synthesized design
+       return tcl_cmds
         
