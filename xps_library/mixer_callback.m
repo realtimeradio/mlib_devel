babce3214e71c7c44cbae480738a59ef9795ae43
Jack <jack@realtimeradio.co.uk>
2023-05-02 12:33:02 +0000

    Don't default mixer type to fine if it is already set to Coarse

--- xps_library/mixer_callback.m
+++ xps_library/mixer_callback.m
@@ -114,7 +114,11 @@ function [] = mixer_callback(gcb, tile, slice, arch)
           msk.getParameter(['t', num2str(tile), '_', prefix, '_dac', num2str(a+1), '_mixer_mode']).TypeOptions = {'Real -> Real', 'I/Q -> Real'};
           if chk_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a+1), '_mixer_mode'], 'I/Q -> Real')
             msk.getParameter(['t', num2str(tile), '_', prefix, '_dac', num2str(a+1), '_mixer_type']).TypeOptions = {'Fine','Coarse'};
-            set_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a+1), '_mixer_type'],'Fine')
+            % Get the current mask value. If not valid, default to Fine
+            curr_val = get_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a+1), '_mixer_type']);
+            if ~any(strcmp({'Fine','Coarse'}, curr_val))
+              set_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a+1), '_mixer_type'],'Fine')
+            end
           else
             msk.getParameter(['t', num2str(tile), '_', prefix, '_dac', num2str(a+1), '_mixer_type']).TypeOptions = {'Bypassed'};
             set_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a+1), '_mixer_type'],'Bypassed');
@@ -126,7 +130,11 @@ function [] = mixer_callback(gcb, tile, slice, arch)
           end
           if chk_param(gcb, mixer_mode_param, 'I/Q -> Real')
             msk.getParameter(['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_type']).TypeOptions = {'Fine','Coarse'};
-            set_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_type'],'Fine')
+            curr_val = get_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_type']);
+            % Get the current mask value. If not valid, default to Fine
+            if ~any(strcmp({'Fine','Coarse'}, curr_val))
+              set_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_type'],'Fine')
+            end
           else %Real->Real
             msk.getParameter(['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_type']).TypeOptions = {'Bypassed'};
             set_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_type'],'Bypassed');
@@ -137,6 +145,11 @@ function [] = mixer_callback(gcb, tile, slice, arch)
       else %odd slices still need to do mixer settings
         if chk_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_mode'], 'I/Q -> Real')
           msk.getParameter(['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_type']).TypeOptions = {'Fine','Coarse'};
+          % Get the current mask value. If not valid, default to Fine
+          curr_val = get_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_type']);
+          if ~any(strcmp({'Fine','Coarse'}, curr_val))
+            set_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_type'],'Fine')
+          end
           set_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_type'],'Fine')
         elseif chk_param(gcb, ['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_mode'], 'Real -> Real')
           msk.getParameter(['t', num2str(tile), '_', prefix, '_dac', num2str(a), '_mixer_type']).TypeOptions = {'Bypassed'};
