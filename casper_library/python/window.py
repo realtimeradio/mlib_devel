f62fb4acc201d8ab6a9e4b3f8b44096eaa30fb70
JackH <jack@realtimeradio.co.uk>
2025-01-18 16:59:54 +0000

    Support DPSS window without MATLAB signal processing toolbox
    Use a MATLAB function call, but....
    
    scipy seems to work when called within matlab, but the
    dpss window function fails with an error relating to eigenvalue
    decomposition. This seems to be related to LAPACK drivers
    and my suspicion is that MATLAB is using its own, which
    aren't playing nicely with scipy.
    
    So, create a complete hack around where the scipy window function is launched
    using the system's python.
    This will fail is the system doesn't have a 'python' command (eg because it
    only has 'python3') or if the system python doesn't have scipy
    installed

--- casper_library/python/window.py
+++ casper_library/python/window.py
@@ -1,5 +1,26 @@
 from math import sin, cos, pi
 
+# Break out of MATLAB's environment
+# Complete hack job
+def run_in_system_python(code):
+    """
+    Executes the given Python code in the system's Python interpreter
+    and returns the output as a list of values.
+
+    Parameters:
+        code (str): Python code to execute. The code must print a JSON-encoded list as output.
+
+    Returns:
+        list: The output values returned by the system Python code execution.
+    """
+    import subprocess
+    import json
+    # Command to run the system Python
+    command = ["python", "-c", code]
+    # Execute the code
+    result = subprocess.run(command, capture_output=True, check=True)
+    return json.loads(result.stdout.strip())
+
 # These window functions duplicated from numpy, because
 # importing numpy here breaks MATLAB ?!?
 
@@ -26,6 +47,11 @@ def hamming(M):
     return [1.0]
   nlist = range(0, M)
   return [(0.54 - 0.46*cos(2.0*pi*n/(M-1))) for n in nlist]
+
+def dpss(M):
+    # Break out of whatever MATLAB environment seems to break scipy's linear algebra
+    script = "import scipy.signal; import json; v = scipy.signal.windows.dpss(%d, 2.0, None, norm='subsample'); vj = json.dumps([x for x in v]); print(vj)" % M
+    return run_in_system_python(script)
 
 def window(wintype, N):
   """
